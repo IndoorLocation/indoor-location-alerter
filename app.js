fa624b30f3965d735aa190bee4890e1d04f4619e
Mathieu Gerard <mathieu@mapwize.io>
2018-04-10 11:07:46 -0700

    Global improvements

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 'use strict';
 
-var async = require('async');
 var turf = require('@turf/turf');
 
 var config = require('./config');
 var mapwize = require('./utils/mapwize');
 var spark = require('./utils/spark');
 
+// Checking that we have all parameters
+if (!config.mapwizeApiKey || !config.mapwizePlaceId || !config.ciscoSparkToken || !config.userId) {
+    console.log('MAPWIZE_API_KEY, MAPWIZE_PLACE_ID, CISCO_SPARK_TOKEN, and USER_ID environment variables are required');
+    process.exit(1);
+}
+
+// To store data and state
+var isInside = null;
+var place = null;
+var placeFeature = null;
+
+// Configuring socket
 var socket = require('socket.io-client')(config.indoorLocationSocketUrl, {
     autoConnect: false,
     query: {
@@ -17,63 +28,71 @@ var socket = require('socket.io-client')(config.indoorLocationSocketUrl, {
 socket.on('connect', function () {
     console.log('Connexion opened to ' + config.indoorLocationSocketUrl + '.');
 });
-socket.connect();
 
+socket.on('error', function (data) {
+    console.log('error');
+    console.log(data);
+});
 
-if (!config.mapwizeApiKey || !config.mapwizePlaceId || !config.ciscoSparkToken || !config.userId) {
-    console.log('MAPWIZE_API_KEY, MAPWIZE_PLACE_ID, CISCO_SPARK_TOKEN, and USER_ID environment variables are required');
-    process.exit(1);
-}
+socket.on('indoorLocationChange', function (data) {
+    var indoorLocation = data.indoorLocation;
+    var indoorLocationPoint = turf.point([indoorLocation.longitude, indoorLocation.latitude]);
+    var _isInside = turf.booleanContains(placeFeature, indoorLocationPoint);
 
-var isInside = null;
-var place = null;
-var placeFeature = null;
+    console.log(indoorLocation);
+    console.log(_isInside);
 
-async.series([
-    /**
-     * Retrieve the room from Mapwize.
-     */
-    function (next) {
-        mapwize.retrievePlace(config.mapwizePlaceId, function (err, _place) {
-            place = _place;
-            placeFeature = turf.feature(_place.geometry);
-            next(err);
-        });
-    },
-    /**
-     * When a new IndoorLocation comes, detect if the user is entering or leaving the room.
-     */
-    function (next) {
-        socket.on('indoorLocationChange', function (data) {
-            var indoorLocation = data.indoorLocation;
-            var indoorLocationPoint = turf.point([indoorLocation.longitude, indoorLocation.latitude]);
-            var _isInside = turf.booleanContains(placeFeature, indoorLocationPoint);
-
-            // Detect any status change
-            if (isInside === null || isInside !== _isInside) {
-                if (_isInside) {
-                    console.log(config.userId + ' just entered \'' + place.name + '\'.');
-                    spark.sendMessage(config.ciscoSparkToPersonEmail, '_' + config.userId + '_ just __entered__ _' + place.name + '_.', function (err) {
-                        if (err) {
-                            console.log('Cisco Spark ERR:', err);
-                        }
-                    });
+    // Detect any status change
+    if (isInside === null) {
+        if (_isInside) {
+            console.log(config.userId + ' is inside \'' + place.name + '\'.');
+            spark.sendMessage(config.ciscoSparkToPersonEmail, '_' + config.userId + '_ is inside _' + place.name + '_.', function (err) {
+                if (err) {
+                    console.log('Cisco Spark ERR:', err);
                 }
-                else {
-                    console.log(config.userId + ' just left \'' + place.name + '\'.');
-                    spark.sendMessage(config.ciscoSparkToPersonEmail, '_' + config.userId + '_ just __left__ _' + place.name + '_.', function (err) {
-                        if (err) {
-                            console.log('Cisco Spark ERR:', err);
-                        }
-                    });
+            });
+        }
+        else {
+            console.log(config.userId + ' is outside \'' + place.name + '\'.');
+            spark.sendMessage(config.ciscoSparkToPersonEmail, '_' + config.userId + '_ is outside _' + place.name + '_.', function (err) {
+                if (err) {
+                    console.log('Cisco Spark ERR:', err);
                 }
+            });
+        }
 
-                isInside = _isInside;
-            }
-        });
+        isInside = _isInside;
+
+    } else if (isInside !== _isInside) {
+        if (_isInside) {
+            console.log(config.userId + ' just entered \'' + place.name + '\'.');
+            spark.sendMessage(config.ciscoSparkToPersonEmail, '_' + config.userId + '_ just __entered__ _' + place.name + '_.', function (err) {
+                if (err) {
+                    console.log('Cisco Spark ERR:', err);
+                }
+            });
+        }
+        else {
+            console.log(config.userId + ' just left \'' + place.name + '\'.');
+            spark.sendMessage(config.ciscoSparkToPersonEmail, '_' + config.userId + '_ just __left__ _' + place.name + '_.', function (err) {
+                if (err) {
+                    console.log('Cisco Spark ERR:', err);
+                }
+            });
+        }
+
+        isInside = _isInside;
     }
-], function (err) {
+});
+
+// Retrieving the place from Mapwize and connecting the socket
+mapwize.retrievePlace(config.mapwizePlaceId, function (err, _place) {
     if (err) {
         console.log('Mapwize ERR', err);
+    } else {
+        place = _place;
+        placeFeature = turf.feature(_place.geometry);
+
+        socket.connect();
     }
 });
